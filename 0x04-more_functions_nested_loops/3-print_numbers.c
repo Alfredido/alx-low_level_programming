f050bf5b7f6973358a0e38881e7aaffb3a06eb2f
root <Alfredido>
2022-10-25 10:14:05 -0700

    my task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0-9
+ * print_numbers - print 0123456789
+ * owned by Bwave ICT
+ * Return: void
  */
+
 void print_numbers(void)
 {
-		int n = 0;
+	char c;
 
-		while (n < 10)
-
-		_putchar(n + 48);
-		n++;
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
